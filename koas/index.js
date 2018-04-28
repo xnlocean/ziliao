c7df05155b7d15990bf0ed099a40a710a4391fcf
chen <635090933@qq.com>
2018-04-28 18:56:57 +0800

    first

--- koas/index.js
+++ koas/index.js
@@ -1,7 +1,18 @@
+'use strict'
+
+const monk = require('monk')
+const fs = require('fs')
+const path = require('path')
+const url = 'admin:123456@localhost:27017/admin'
+const db = monk(url)
+
+const collection = db.get('test')
 const Koa = require('koa')
 const app = new Koa()
-app.use( async (ctx) => {
-    ctx.body = 'hello koa2'
+app.use( async ctx => {
+    const test = await collection.count()
+    db.close()
+    ctx.body = test
 })
 app.listen(3000)
-console.log('[demo] start-quick is starting at port 3000')
+console.log('app starting at port 3000')
